8a6337867300b3bc00c6a5e64fd8f4b3f2045796
mokuo <tennis10988.yk@gmail.com>
2020-08-10 18:37:06 +0900

    Yokohama::FacilitySelectionPage#click_park(park_name) 実装

--- app/pages/yokohama/facility_selection_page.rb
+++ app/pages/yokohama/facility_selection_page.rb
@@ -2,5 +2,30 @@
 
 module Yokohama
   class FacilitySelectionPage < ApplicationPage
+    class ParkNotFoundError < StandardError; end
+
+    def initialize
+      @current_page = 1
+    end
+
+    def click_park(park_name)
+      find_and_click_button(park_name)
+      Yokohama::FieldTypeSelectionPage.new
+    end
+
+    private
+
+    def find_and_click_button(button_text)
+      raise ParkNotFoundError if @current_page == 3
+
+      if page.has_button?(button_text)
+        click_button(button_text)
+      else
+        click_button("次のページ")
+        @current_page += 1
+
+        find_and_click_button(button_text)
+      end
+    end
   end
 end
---
+++ app/pages/yokohama/field_type_selection_page.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Yokohama
+  class FieldTypeSelectionPage < ApplicationPage
+  end
+end

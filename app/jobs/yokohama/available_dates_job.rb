859f2b4a69b0111a226567bbf309e6c22a0bc78f
mokuo <tennis10988.yk@gmail.com>
2020-08-14 22:52:54 +0900

    refactor AvailableDatesJob

--- app/jobs/yokohama/available_dates_job.rb
+++ app/jobs/yokohama/available_dates_job.rb
@@ -4,6 +4,15 @@ module Yokohama
   class AvailableDatesJob < Gush::Job
     def perform
       park_name = params[:park_name]
+      available_dates = get_available_dates(park_name)
+      workflow = next_workflow_class.create(park_name, available_dates)
+      workflow.start!
+      workflow
+    end
+
+    private
+
+    def get_available_dates(park_name)
       current_month_page = Yokohama::TopPage.new
                                             .click_check_availability
                                             .click_sports
@@ -13,13 +22,7 @@ module Yokohama
       available_dates = current_month_page.available_dates
       next_month_page = current_month_page.click_next_month
       available_dates.concat(next_month_page.available_dates) unless next_month_page.error_page?
-
-      workflow = next_workflow_class.create(park_name, available_dates)
-      workflow.start!
-      workflow
     end
-
-    private
 
     def next_workflow_class
       params[:next_workflow_class] || AvailableDateTimesWorkflow
--- spec/jobs/yokohama/available_dates_job_spec.rb
+++ spec/jobs/yokohama/available_dates_job_spec.rb
@@ -27,18 +27,23 @@ RSpec.describe Yokohama::AvailableDatesJob, type: :feature do
     end
     let!(:job) { described_class.new(params) }
 
-    it "指定された公園の利用可能日を収集すること" do
+    it "指定された公園の利用可能日を取得する" do
       test_workflow = collect_available_dates
       expect(test_workflow.park_name).to eq "富岡西公園"
-
-      available_dates = test_workflow.available_dates
-      expect(available_dates).to be_a(Array)
-      # NOTE: 平日も含めれば、一日も空きがないということはないだろう
-      expect(available_dates.count).to be > 0
-      expect(available_dates).to all be_a(Date)
     end
 
-    it "AvailableDateTimesWorkflow をスタートすること" do
+    it "利用可能日が取得できている" do
+      # NOTE: 平日も含めれば、一日も空きがないということはないだろう
+      test_workflow = collect_available_dates
+      expect(test_workflow.available_dates.count).to be > 0
+    end
+
+    it "Date 型の配列を返す" do
+      test_workflow = collect_available_dates
+      expect(test_workflow.available_dates).to all be_a(Date)
+    end
+
+    it "AvailableDateTimesWorkflow をスタートする" do
       test_workflow = collect_available_dates
       expect(test_workflow.started).to be true
     end

75f02e2295bb9944ef52b09bf9aecd343aabe1e2
mokuo <tennis10988.yk@gmail.com>
2020-08-20 21:38:21 +0900

    cosmetic change: メソッド名、変数名を直感的にわかりやすくする

--- app/pages/yokohama/reservation_frame_selection_page.rb
+++ app/pages/yokohama/reservation_frame_selection_page.rb
@@ -5,15 +5,15 @@ module Yokohama
     class MissingSelectButtonError < StandardError; end
 
     def initialize
-      check_page_status!
+      check_logged_in!
     end
 
     # rubocop:disable Metrics/MethodLength
     def reservation_frames
       result = []
 
-      tennis_court_row_elements.each do |tennis_court_row_element|
-        columns = tennis_court_row_element.all("td")
+      tennis_court_tr_elements.each do |tennis_court_tr_element|
+        columns = tennis_court_tr_element.all("td")
         tennis_court_name = columns[0].text
         reservation_frame_elements = columns.drop(2) # 「施設」「定員」列を取り除く
 
@@ -32,9 +32,9 @@ module Yokohama
     # rubocop:enable Metrics/MethodLength
 
     def click_reservation_frame(reservation_frame)
-      tennis_court_row_element = find_tennis_court_row_element(reservation_frame.tennis_court_name)
+      tennis_court_tr_element = find_tennis_court_tr_element(reservation_frame.tennis_court_name)
       # NOTE: 部分一致
-      input_element = tennis_court_row_element.find(
+      input_element = tennis_court_tr_element.find(
         "td input[onclick*='\\'#{reservation_frame.date_str}\\',\\'#{reservation_frame.time_str}\\'']"
       )
       input_element.click
@@ -47,15 +47,15 @@ module Yokohama
       Yokohama::ReservationConfirmationPage.new
     end
 
-    def find_tennis_court_row_element(tennis_court_name)
-      tennis_court_row_elements.find do |e|
+    def find_tennis_court_tr_element(tennis_court_name)
+      tennis_court_tr_elements.find do |e|
         e.find("td:first-child").text == tennis_court_name
       end
     end
 
     private
 
-    def check_page_status!
+    def check_logged_in!
       raise MissingSelectButtonError unless logged_in?
     end
 
@@ -63,7 +63,7 @@ module Yokohama
       %w[予約済 ×].include?(text)
     end
 
-    def tennis_court_row_elements
+    def tennis_court_tr_elements
       table_row_elements = all("table#tbl_time tr")
       result = table_row_elements.drop(1) # テーブルのヘッダーを取り除く
       result.pop # 空行を取り除く

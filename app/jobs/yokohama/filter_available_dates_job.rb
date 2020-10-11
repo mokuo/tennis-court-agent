76b0a2165cd123a776009a60419d0119aac16403
mokuo <tennis10988.yk@gmail.com>
2020-10-11 18:24:53 +0900

    fix rspec & rubocop

--- app/jobs/yokohama/filter_available_dates_job.rb
+++ app/jobs/yokohama/filter_available_dates_job.rb
@@ -7,12 +7,12 @@ module Yokohama
     queue_as :yokohama
 
     def self.dispatch_jobs(identifier, park_name, dates)
-      avaiable_dates = dates.map { |date| AvailableDate.new(date) }
+      available_dates = dates.map { |date| AvailableDate.new(date) }
       Rails.logger.info(
         {
           identifier: identifier,
           park_name: park_name,
-          avaiable_dates: avaiable_dates
+          available_dates: available_dates
         }
       )
     end
--- spec/app/jobs/persist_event_job_spec.rb
+++ spec/app/jobs/persist_event_job_spec.rb
@@ -4,30 +4,38 @@ require Rails.root.join("domain/models/availability_check_identifier")
 require Rails.root.join("domain/events/domain_event")
 
 class TestDomainEvent < DomainEvent
-  attribute :some_attribute
-
-  private
+  attribute :some_attribute, :string
 
   def subscribers
     []
+  end
+
+  def to_hash
+    attributes.symbolize_keys.merge(name: self.class.to_s)
+  end
+
+  def self.from_hash(hash)
+    new(
+      availability_check_identifier: hash[:availability_check_identifier],
+      published_at: hash[:availability_check_identifier],
+      some_attribute: hash[:some_attribute]
+    )
   end
 end
 
 RSpec.describe PersistEventJob, type: :job do
   describe "#perform" do
     let!(:identifier) { AvailabilityCheckIdentifier.build }
-    let!(:domain_event) do
+    let!(:test_domain_event) do
       TestDomainEvent.new(
-        {
-          availability_check_identifier: identifier,
-          published_at: Time.current,
-          some_attribute: "hoge"
-        }
+        availability_check_identifier: identifier,
+        published_at: Time.current,
+        some_attribute: "hoge"
       )
     end
 
     it "ドメインイベントを永続化する" do
-      expect { described_class.new.perform(domain_event) }.to change(Event, :count).from(0).to(1)
+      expect { described_class.new.perform(test_domain_event.to_hash) }.to change(Event, :count).from(0).to(1)
     end
   end
 end

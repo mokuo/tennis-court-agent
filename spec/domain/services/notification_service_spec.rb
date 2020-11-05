384c0853b27ad0d2e015c90bbee19e77f1175edd
mokuo <tennis10988.yk@gmail.com>
2020-11-05 22:26:02 +0900

    NotificationService 実装

--- spec/domain/services/notification_service_spec.rb
+++ spec/domain/services/notification_service_spec.rb
@@ -4,19 +4,19 @@ require Rails.root.join("domain/services/notification_service")
 require Rails.root.join("domain/models/yokohama/reservation_frame")
 
 class ClientMock
-  attr_reader :message_sent
+  attr_reader :sent_message
 
   def send(message)
-    @message_sent = message
+    @sent_message = message
   end
 end
 
 RSpec.describe NotificationService do
-  # TODO: ReservationStatusChecked イベントの attributes から取る？
-  xdescribe "#send_availabilities" do
+  describe "#send_availabilities" do
     let!(:reservation_frame_1) do
       Yokohama::ReservationFrame.new(
         {
+          park_name: "公園名１",
           tennis_court_name: "テニスコート１",
           start_date_time: Time.zone.local(2020, 8, 22, 11),
           end_date_time: Time.zone.local(2020, 8, 22, 13),
@@ -27,6 +27,7 @@ RSpec.describe NotificationService do
     let!(:reservation_frame_2) do
       Yokohama::ReservationFrame.new(
         {
+          park_name: "公園名１",
           tennis_court_name: "テニスコート２",
           start_date_time: Time.zone.local(2020, 8, 22, 13),
           end_date_time: Time.zone.local(2020, 8, 22, 15),
@@ -39,16 +40,16 @@ RSpec.describe NotificationService do
       <<~MSG
         横浜市のテニスコートの空き状況です。
 
-        - 富岡西公園 テニスコート１ 2020/08/22（土） 11:00~13:00 今すぐ予約可能
-        - 富岡西公園 テニスコート２ 2020/08/22（土） 13:00~15:00 翌日７時に予約可能
+        - 公園名１ テニスコート１ 2020/08/22（土） 11:00~13:00 今すぐ予約可能
+        - 公園名１ テニスコート２ 2020/08/22（土） 13:00~15:00 翌日７時に予約可能
       MSG
     end
 
     it "テニスコートの空いている予約枠を通知する" do
       client_mock = ClientMock.new
       notification_service = described_class.new(client_mock)
-      notification_service.send_availabilities("横浜市", "富岡西公園", reservation_frames)
-      expect(client_mock.message_sent).to eq expected_message
+      notification_service.send_availabilities("横浜市", reservation_frames)
+      expect(client_mock.sent_message).to eq expected_message
     end
   end
 end

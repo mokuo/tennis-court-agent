abb62e6f80a359ba38164efa20eb78b128fe9d28
mokuo <tennis10988.yk@gmail.com>
2020-11-10 09:22:07 +0900

    予約枠の順番を揃えて通知する

--- domain/services/notification_service.rb
+++ domain/services/notification_service.rb
@@ -17,14 +17,20 @@ class NotificationService
 
   attr_reader :client
 
-  # TODO: ReservationStatusChecked イベントの attributes から取る？
   def build_message(organization_name, reservation_frames)
     msg = "#{organization_name}のテニスコートの空き状況です。\n\n"
 
-    reservation_frames.each do |reservation_frame|
+    rfs = sort_reservation_frames(reservation_frames)
+    rfs.each do |reservation_frame|
       msg += "- #{reservation_frame.to_human}\n"
     end
 
     msg
+  end
+
+  def sort_reservation_frames(reservation_frames)
+    rfs = reservation_frames.sort_by(&:start_date_time)
+    rfs = rfs.sort_by(&:tennis_court_name)
+    rfs.sort_by(&:park_name)
   end
 end

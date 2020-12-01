e4339b45be2ae17b47b00ba583689a4f6b91cd26
mokuo <tennis10988.yk@gmail.com>
2020-12-01 21:15:41 +0900

    POST /api/wakeup エンドポイント追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ require "sidekiq/web"
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
+
+  namespace :api, format: :json do
+    resources :wakeup, only: [:create]
+  end
 end

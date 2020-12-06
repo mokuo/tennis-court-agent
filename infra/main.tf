69c1bfad2aa97c1d7d7c7f51f2449c0927f34e49
mokuo <tennis10988.yk@gmail.com>
2020-12-06 18:48:54 +0900

    リトライ回数を１に設定する

--- infra/main.tf
+++ infra/main.tf
@@ -4,5 +4,18 @@ provider "google" {
 }
 
 resource "google_cloud_scheduler_job" "job" {
+  name             = "wakeup-tennis-court-agent"
+  description      = "スリープしている tennis-court-agent を起こす"
+  schedule         = "55 20 * * *"
+  time_zone        = "Asia/Tokyo"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://tennis-court-agent.herokuapp.com/api/wakeup" # 環境変数にしたい
+  }
 }
 

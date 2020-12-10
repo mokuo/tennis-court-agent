66841b409582f0364b22e410f6a4c41ffee67211
mokuo <tennis10988.yk@gmail.com>
2020-12-10 21:13:38 +0900

    bin/rails g controller ReservationFrames index

---
+++ app/controllers/reservation_frames_controller.rb
@@ -0,0 +1,4 @@
+class ReservationFramesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  get 'reservation_frames/index'
   mount Sidekiq::Web => "/sidekiq"
 
   namespace :api, format: :json do

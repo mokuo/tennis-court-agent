422101549cda3779165b289dec3545c76e04ab9c
mokuo <tennis10988.yk@gmail.com>
2020-12-19 11:36:56 +0900

    7時前に起こすジョブを登録

--- infra/main.tf
+++ infra/main.tf
@@ -3,11 +3,27 @@ provider "google" {
   region  = "asia-northeast1"
 }
 
-resource "google_cloud_scheduler_job" "job" {
-  name             = "wakeup-tennis-court-agent"
-  description      = "スリープしている tennis-court-agent を起こす"
-  schedule         = "55 20 * * *"
-  time_zone        = "Asia/Tokyo"
+resource "google_cloud_scheduler_job" "wakeup_job_21" {
+  name        = "wakeup-tennis-court-agent21"
+  description = "スリープしている tennis-court-agent を起こす"
+  schedule    = "55 20 * * *" # 21時前
+  time_zone   = "Asia/Tokyo"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://tennis-court-agent.herokuapp.com/api/wakeup" # 環境変数にしたい
+  }
+}
+
+resource "google_cloud_scheduler_job" "wakeup_job_7" {
+  name        = "wakeup-tennis-court-agent7"
+  description = "スリープしている tennis-court-agent を起こす"
+  schedule    = "55 6 * * *" # 7時前
+  time_zone   = "Asia/Tokyo"
 
   retry_config {
     retry_count = 1

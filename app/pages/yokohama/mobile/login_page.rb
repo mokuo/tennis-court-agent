e2f790ff293bf7d04c472787b14564f9f75d64c1
mokuo <tennis10988.yk@gmail.com>
2021-04-20 21:47:22 +0900

    Yokohama::Mobile::LoginPage 実装

--- app/pages/yokohama/mobile/login_page.rb
+++ app/pages/yokohama/mobile/login_page.rb
@@ -3,6 +3,16 @@
 module Yokohama
   module Mobile
     class LoginPage < BasePage
+      def login(
+        user_number = Rails.application.credentials.yokohama![:user_number],
+        password = Rails.application.credentials.yokohama![:password]
+      )
+        find("input[name='ID']").set(user_number)
+        find("input[name='PWD']").set(password)
+        find("input[value='ﾛｸﾞｲﾝ']").click
+
+        Yokohama::Mobile::MessagePage.new
+      end
     end
   end
 end
---
+++ app/pages/yokohama/mobile/message_page.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Yokohama
+  module Mobile
+    class MessagePage < BasePage
+    end
+  end
+end
